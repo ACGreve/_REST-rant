b5847e845781ee15dedfe92f67272a6084999afe
ACGreve <greve5921@gmail.com>
2023-07-28 22:21:27 -0400

    Working on comments, created seed, having issues with .create

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     .then(() => console.log('DB connected'))
     .catch(err => console.error(err));
     
-module.exports.Place = require('./controllers/places')
+module.exports.Place = require('./models/places')
 module.exports.Comment = require('./models/comment')
 
 // Listen for Connections
--- seeders/seed-comments.js
+++ seeders/seed-comments.js
@@ -3,7 +3,8 @@ const db = require('../models')
 // To use await, we need an async function.
 async function seed() {
     // Get the place, H-Thai-ML
-    let place = await db.Place.findOne({ name: 'H-Thai-ML' })
+    let place = await db.Place.findOne({ name: "H-Thai-ML" })
+    console.log(place)
 
     // Create a fake sample comment.
     let comment = await db.Comment.create({
