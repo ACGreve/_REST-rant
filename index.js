26b033cc1225980d6e33b3b850556c5e1a3fc113
ACGreve <greve5921@gmail.com>
2023-07-28 21:10:23 -0400

    added comments seed (not working)

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     .catch(err => console.error(err));
     
 module.exports.Place = require('./controllers/places')
+module.exports.Comment = require('./models/comment')
 
 // Listen for Connections
 app.listen(process.env.PORT)
